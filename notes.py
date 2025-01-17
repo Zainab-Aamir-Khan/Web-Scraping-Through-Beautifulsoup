ce0839802e052d88f592d15a5d29aed0e1138f95
Zainab Aamir <zaamir427@gmail.com>
2025-01-17 22:25:41 +0500

    notes

---
+++ notes.py
@@ -0,0 +1,5 @@
+# print(soup.prettify())                          #prettifier will make your html code look prettier
+# print(content)                                  #print all the html content
+#lxml for parsing the html data
+ #r --> for read
+

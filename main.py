e9423b6e79d7de8b9b90a4e12a384cdbb82081e6
Zainab Aamir <zaamir427@gmail.com>
2025-01-17 23:23:02 +0500

    second commit

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ with open('home.html', 'r') as html_file:
     content = html_file.read() 
 
     soup = BeautifulSoup(content, 'lxml')           
-    tags = soup.find_all("h5")
-    print(tags)
+    coursesHtmlTags = soup.find_all('h5')
+
+    for courses in coursesHtmlTags:
+        print(courses.text)
+
+    coursesCards = soup.find_all('div', class_='card')
+    for cards in coursesCards:
+        print(cards.h5.text)
+        print(cards.a.text)
+        
+        #we can also put these in another variable and print those variables for example:
+        
--- notes.py
+++ notes.py
@@ -1,5 +1,12 @@
 # print(soup.prettify())                          #prettifier will make your html code look prettier
+
 # print(content)                                  #print all the html content
+
 #lxml for parsing the html data
+
  #r --> for read
+
+# tags = soup.find_all("h5")  --> this _all will find all the h5 tags in the html file
+#     print(tags)
+
 

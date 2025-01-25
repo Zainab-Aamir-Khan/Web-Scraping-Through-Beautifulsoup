ef2df3b556b6f82205db536f45201f61a2d336df
Zainab Aamir <zaamir427@gmail.com>
2025-01-25 23:28:09 +0500

    added a new practice

---
+++ app.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get('https://books.toscrape.com/')
+soup = BeautifulSoup(response.content, 'lxml')
+books = soup.find_all('li', class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
+for book in books:
+    bookName = book.h3.a['title']
+    print(bookName)
+
+
+
+
